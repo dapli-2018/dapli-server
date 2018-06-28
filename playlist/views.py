f8adc11f9417c5c413d18aaa558d75e7ff77b12c
machenity <machenity@gmail.com>
2018-06-29 02:30:05 +0900

    Add view: Host access this view to make a group playlist

--- playlist/views.py
+++ playlist/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import json
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from playlist.models import SongInfo, GroupPlaylist
 
-# Create your views here.
+
+@api_view(['POST'])
+def post_initial_playlist(request):
+    songs = request.POST.get('songs')
+    if type(songs) is not list:
+        return JsonResponse({'code': status.HTTP_412_PRECONDITION_FAILED})
+
+    key = GroupPlaylist.objects.order_by('key').first()
+    indices = json.dumps(list(range(len(songs))))
+    if key == None:
+        key = 1000
+        g = GroupPlaylist.objects.create(key=key, indices=str(indices))
+        g.save()
+    else:
+        try:
+            g = GroupPlaylist.objects.create(key=key+1, indices=str(indices))
+            g.save()
+        except ValidationError: # key를 9999까지 발급했을 때
+            try:
+                g = GroupPlaylist.objects.create(key = 1000, indices=str(indices))
+                g.save()
+            except IntegrityError: # 너무 많은 접속자로 잠시 제한
+                return JsonResponse({'code': status.HTTP_503_SERVICE_UNAVAILABLE})
+
+    for i in range(songs):
+        songinfo = SongInfo.objects.create(
+            key=g,
+            index=i,
+            title=songs[i][0],
+            artist=songs[i][1],
+            album=songs[i][2]
+        )
+        songinfo.save()
+
+    return JsonResponse({'code': status.HTTP_201_CREATED, 'key': key})
+
