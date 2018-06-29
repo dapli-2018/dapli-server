02a010028e322770ff869dc92ea7ab1eca86599f
machenity <machenity@gmail.com>
2018-06-29 11:32:25 +0900

    Add url for GuestView

--- playlist/urls.py
+++ playlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from playlist import views
 
 urlpatterns = [
-    path('host', views.HostView.as_view())
+    path('host', views.HostView.as_view()),
+    path('guest', views.GuestView.as_view())
 ]
