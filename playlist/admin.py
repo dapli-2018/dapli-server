bb7d5daf97a7b8a6b4029e60c2132304a99a1c5b
machenity <machenity@gmail.com>
2018-06-30 00:35:44 +0900

    Add AuthKey on Admin page

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import SongInfo, Playlist
+from .models import SongInfo, Playlist, AuthKey
 
 # Register your models here.
 admin.site.register(SongInfo)
 admin.site.register(Playlist)
+admin.site.register(AuthKey)
