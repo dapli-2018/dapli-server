7c8b923a63b1858c16e65008e6810ebddca41acd
machenity <machenity@gmail.com>
2018-06-29 20:56:14 +0900

    Change model scheme: delete key & add tag

--- playlist/models.py
+++ playlist/models.py
@@ -4,10 +4,10 @@ from django.core.validators import (MaxValueValidator, MinValueValidator)
 # Create your models here.
 
 class Playlist(models.Model):
-    key = models.IntegerField(unique=True, validators=[MaxValueValidator(9999), MinValueValidator(1000)])
     title = models.CharField(max_length=90)
     date = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=300)
+    tag = models.CharField(max_length=90)
 
 
 class SongInfo(models.Model):
@@ -18,3 +18,7 @@ class SongInfo(models.Model):
     album = models.TextField(default="", max_length=90)
     is_on_playlist = models.BooleanField(default=True)
     is_played = models.BooleanField(default=False)
+
+class AuthKey(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    key = models.IntegerField(unique=True, validators=[MaxValueValidator(9999), MinValueValidator(1000)])
